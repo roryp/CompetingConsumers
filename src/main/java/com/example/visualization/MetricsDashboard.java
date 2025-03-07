3678efe6c3414202536cacacf1c2aba5cc9c11a6
Rory Preddy <rorypreddy@gmail.com>
2025-03-07 14:53:37 +0200

    Remove unused imports in MetricsDashboard.java

--- src/main/java/com/example/visualization/MetricsDashboard.java
+++ src/main/java/com/example/visualization/MetricsDashboard.java
@@ -20,9 +20,7 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
 public class MetricsDashboard {
     private static final Logger logger = LoggerFactory.getLogger(MetricsDashboard.class);
